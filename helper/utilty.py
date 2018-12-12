Fixed OpenCV-based channel conversion, smoke scaling is fine now

helper/utilty.py
@@ -127,32 +127,31 @@ def save_image(filename, image, qua=95, print_console=False):
 
 def convert_rgb_to_y(image):
     ycbcr = cv2.cvtColor(image, cv2.COLOR_RGB2YCR_CB)
-    only_Cb, only_Cr, only_y = cv2.split(ycbcr)
+    only_y, only_Cr, only_Cb = cv2.split(ycbcr)
     only_y = np.atleast_3d(only_y)
     return only_y.astype('float32')
 
 
+def convert_ycbcr_to_y_cbcr(ycbcr):
+    only_y, only_Cr, only_Cb = cv2.split(ycbcr)
+    only_y = np.atleast_3d(only_y)
+
+    return only_y.astype('float32'), (only_Cb.astype('float32'), only_Cr.astype('float32'))
+    
+
 def convert_rgb_to_ycbcr(image):
     ycbcr = cv2.cvtColor(image, cv2.COLOR_RGB2YCR_CB)
-    return ycbcr
+    return ycbcr.astype('float32')
 
 
 def convert_ycbcr_to_rgb(ycbcr_image):
-    image = cv2.cvtColor(ycbcr_image, cv2.COLOR_YCR_CB2RGB)
+    image = cv2.cvtColor(ycbcr_image.clip(0, 255).astype(np.uint8), cv2.COLOR_YCR_CB2RGB)
     return image
 
 
 def convert_y_and_cbcr_to_rgb(y_image, cbcr_image):
-    if len(y_image.shape) <= 2:
-        y_image = y_image.reshape[y_image.shape[0], y_image.shape[1], 1]
-
-    if len(y_image.shape) == 3 and y_image.shape[2] == 3:
-        y_image = y_image[:, :, 0:1]
-
-    ycbcr_image = np.zeros([y_image.shape[0], y_image.shape[1], 3])
-    ycbcr_image[:, :, 0] = y_image[:, :, 0]
-    ycbcr_image[:, :, 1:3] = cbcr_image[:, :, 0:2]
-
+    only_Cb, only_Cr = cbcr_image
+    ycbcr_image = cv2.merge((y_image, only_Cr, only_Cb))
     return convert_ycbcr_to_rgb(ycbcr_image)
 
 

sr0.py
@@ -17,56 +17,23 @@ args.flags.DEFINE_string("file_glob", "", "Target filenames pattern")
 FLAGS = args.get()
 
 
-def cv_convert_rgb_to_y(image):
-    ycbcr = cv2.cvtColor(image, cv2.COLOR_RGB2YCR_CB)
-    only_Cb, only_Cr, only_y = cv2.split(ycbcr)
-    return only_y
-
-
 class Upscaler(DCSCN.SuperResolution):
     def do_for_file(self, file_path, output_folder="output"):
-        org_image = cv2.imread(file_path)                   #org_image = util.load_image(file_path)
+        org_image = cv2.imread(file_path)
         assert len(org_image.shape) >= 3 and org_image.shape[2] == 3 and self.channels == 1
 
-        input_ycbcr_image = cv2.cvtColor(org_image, cv2.COLOR_RGB2YCR_CB)
-        only_Cb, only_Cr, input_y_image = cv2.split(input_ycbcr_image)
+        input_y_image = util.convert_rgb_to_y(org_image)
 
-        big_blurry_input_image = cv2.resize(org_image, (org_image.shape[1] * self.scale, org_image.shape[0] * self.scale))
-        big_blurry_input_ycbcr_image = cv2.cvtColor(big_blurry_input_image, cv2.COLOR_RGB2YCR_CB)
-        bbi_only_Cb, bbi_only_Cr, bbi_input_y_image = cv2.split(big_blurry_input_ycbcr_image)
+        big_blurry_input_image = util.resize_image_by_pil(org_image, self.scale)
+        big_blurry_input_ycbcr_image = util.convert_rgb_to_ycbcr(big_blurry_input_image)
+        bbi_input_y_image, bbi_input_cbcr_image = util.convert_ycbcr_to_y_cbcr(big_blurry_input_ycbcr_image)
 
         output_y_image = self.do(input_y_image, bbi_input_y_image)
 
-        output_ycbcr_image = cv2.merge((bbi_only_Cb, bbi_only_Cr, output_y_image))
-        output_image = cv2.cvtColor(output_ycbcr_image, cv2.COLOR_YCR_CB2RGB)
+        output_image = util.convert_y_and_cbcr_to_rgb(output_y_image, bbi_input_cbcr_image)
 
         target_path = os.path.basename(file_path)
-        cv2.imwrite(os.path.join(output_folder, target_path), output_image)      #util.save_image(os.path.join(output_folder, target_path), image)
-
-    def do(self, y_input_image, bicubic_y_input_image=None):
-
-        h, w = y_input_image.shape[:2]
-        ch = y_input_image.shape[2] if len(y_input_image.shape) > 2 else 1
-
-        assert bicubic_y_input_image is not None
-
-        if self.max_value != 255.0:
-            y_input_image *= self.max_value / 255.0
-            bicubic_y_input_image *= self.max_value / 255.0
-
-        assert self.self_ensemble == 1
-
-        y = self.sess.run(self.y_, feed_dict={self.x: y_input_image.reshape(1, h, w, ch),
-                                              self.x2: bicubic_y_input_image.reshape(1, self.scale * h,
-                                                                                     self.scale * w, ch),
-                                              self.dropout: 1.0, self.is_training: 0})
-        output_image = y[0,:,:,0]
-
-        if self.max_value != 255.0:
-            output_image *= 255.0 / self.max_value
-
-        return output_image.astype(np.uint8)
-
+        cv2.imwrite(os.path.join(output_folder, target_path), output_image)
 
 def upscale(model, fname, output):
     start = time.time()